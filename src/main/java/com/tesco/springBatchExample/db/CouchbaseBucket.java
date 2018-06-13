2478e62fae1fe5b9171d6889275354e1946cb688
Rituj Kumar <rituj.kumar@tesco.com>
2018-06-13 14:46:21 +0530

    removing the Couchbase manger Classes

--- src/main/java/com/tesco/springBatchExample/db/CouchbaseBucket.java
+++
@@ -1,5 +0,0 @@
-package com.tesco.springBatchExample.db;
-
-public enum CouchbaseBucket {
-	Batch
-}
--- src/main/java/com/tesco/springBatchExample/items/CustomItemWriter.java
+++ src/main/java/com/tesco/springBatchExample/items/CustomItemWriter.java
@@ -6,26 +6,18 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.couchbase.client.java.Bucket;
-import com.tesco.springBatchExample.db.CouchBaseConnectionManager;
-import com.tesco.springBatchExample.db.CouchBaseRepository;
-import com.tesco.springBatchExample.db.CouchbaseBucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
 
 public class CustomItemWriter<User> implements ItemWriter<User> {
 
 	@Autowired
-	CouchBaseConnectionManager couchBaseConnectionManager;
-
-	@Autowired
-	CouchBaseRepository couchBaseRepository;
+	Bucket bucket;
 
 	@Override
 	public void write(List<? extends User> items) throws Exception {
 
-		Bucket bucket = couchBaseConnectionManager.getBucket(CouchbaseBucket.Batch.toString());
-
-		for (User user : items) {
-			couchBaseRepository.create(user, bucket, Class.class);
-		}
-
+			JsonDocument doc = JsonDocument.create("DocumentId", JsonObject.create().put("user", items));
+			bucket.upsert(doc);
 	}
 }
